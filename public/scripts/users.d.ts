185ed9d736576de227ac87c27a60db59e8f3cc33
Dan Silver <dannysilver3@gmail.com>
2016-12-23 14:41:38 -0800

    Reremoved generated js

--- public/scripts/users.d.ts
+++
@@ -1,4 +0,0 @@
-import { User } from './common';
-export declare let completeLogin: (existingUser?: User) => Promise<User>;
-export declare let saveToDb: (user: User, includeActions: boolean, includeMakerKey: boolean) => firebase.Promise<any>;
-export declare let getFromGoogleSignin: () => Promise<User>;
